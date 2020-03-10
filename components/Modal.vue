6b189e6ec79177b55dfcf9ed2951033514f54a42
unknown <austin.akers5@gmail.com>
2020-03-10 00:27:16 -0500

    created scaffold for modal component

---
+++ components/Modal.vue
@@ -0,0 +1,36 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              default body
+            </slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              default footer
+              <button @click="$emit('close')" class="modal-default-button">
+                OK
+              </button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -21,14 +21,21 @@
           </div>
         </div>
       </div>
+      <button
+        @click="isOpen = true"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Open Modal
+      </button>
 
       <h2 class="text-3xl mb-1">Projects</h2>
       <div class="flex flex-wrap mb-4">
-        <div class="sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/4 h-auto">
+        <div class="sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/4 h-auto mt-4">
           <project-card />
         </div>
       </div>
     </div>
+    <modal v-if="isOpen" @close="isOpen = false"></modal>
   </div>
 </template>
 <script>
@@ -36,15 +43,23 @@ import Navbar from '~/components/Navbar'
 import WorkHistoryCard from '~/components/WorkHistoryCard'
 import ProjectCard from '~/components/ProjectCard'
 import workHistoryData from '~/data/workHistory.json'
+import Modal from '~/components/Modal'
 export default {
   components: {
     Navbar,
     WorkHistoryCard,
-    ProjectCard
+    ProjectCard,
+    Modal
   },
   data() {
     return {
-      workHistory: workHistoryData
+      workHistory: workHistoryData,
+      isOpen: false
+    }
+  },
+  methods: {
+    showModal() {
+      this.isOpen = true
     }
   }
 }
