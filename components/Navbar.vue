036fcce6e438317eeb2ae351b5398a006390fd17
unknown <austin.akers5@gmail.com>
2020-04-23 14:58:42 -0500

    finished portfolio

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,8 +5,9 @@
         Austin Akers
       </nuxt-link>
     </div>
-    <!-- <div class="block lg:hidden">
+    <div class="block lg:hidden">
       <button
+        @click="isOpen = !isOpen"
         class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
       >
         <svg
@@ -18,20 +19,17 @@
           <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
         </svg>
       </button>
-    </div> -->
-    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    </div>
+    <div
+      :class="isOpen ? 'block' : 'hidden'"
+      class="w-full flex-grow lg:flex lg:items-center lg:w-auto"
+    >
       <div class="text-sm lg:flex-grow">
         <nuxt-link
           to="/portfolio"
           class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
         >
           Portfolio
-        </nuxt-link>
-        <nuxt-link
-          to="/blog"
-          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-        >
-          Blog
         </nuxt-link>
         <nuxt-link
           to="/contact"
@@ -43,3 +41,12 @@
     </div>
   </nav>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    }
+  }
+}
+</script>
