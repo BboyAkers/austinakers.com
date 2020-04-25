fc38101294da07ffca6fa6b4d26a5aa4e56dc05a
unknown <austin.akers5@gmail.com>
2020-04-25 09:33:23 -0500

    pushing analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [{ src: '~plugins/ga.js', mode: 'client' }],
   /*
    ** Nuxt.js dev-modules
    */
