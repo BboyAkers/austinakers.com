92f360d409fd7486188774b98acb771e7f9085b6
Austin Akers <austin.akers5@gmail.com>
2026-09-22 17:44:56 -0400

    pushed config updated gitignore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,17 @@ export default defineNuxtConfig({
     '@nuxt/image',
   ],
   css: ['~/assets/css/main.css'],
+  routeRules: {
+    '/': { prerender: true },
+    '/blog': { prerender: true },
+    '/blog/**': { prerender: true },
+    '/work': { prerender: true },
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+    },
+  },
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
 })
